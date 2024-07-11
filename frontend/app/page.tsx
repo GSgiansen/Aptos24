947a0d5356d389582d7280d96143d43df118beae
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2024-07-11 23:10:11 +0800

    Wrap layout and add aptos wallet provider

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import Image from "next/image";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Aptos Ticket Master</h1>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      </div>
+      <WalletSelector />
     </main>
   );
 }
