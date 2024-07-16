2849b4dd1ca7d2758d6eed1396f77ce9ea7ba90e
weeweh <e0969150@u.nus.edu>
2024-07-17 01:13:51 +0800

    Add verify ticket

--- aptosticketmaster/frontend/pages/Tickets/TicketCard.tsx
+++ aptosticketmaster/frontend/pages/Tickets/TicketCard.tsx
@@ -2,10 +2,13 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button"; // Assume you have a Button component
 import { Ticket } from "@/hooks/getEventsAndTickets";
 import { useEffect, useState } from "react";
+import { verifyTicket } from "@/hooks/verifyTicket";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 const TicketCard = (ticket: Ticket) => {
   const [metadata, setMetadata] = useState<TicketMetadata | null>(null);
-
+  const wallet = useWallet();
+  
   interface TicketMetadata {
     image: string;
     external_url: string;
@@ -33,7 +36,7 @@ const TicketCard = (ticket: Ticket) => {
         </CardHeader>
         <CardContent>
             <img src={metadata?.image || "" } alt={ticket.event_name} />
-            <Button>Verify Ticket</Button>
+              <Button onClick={() => verifyTicket(ticket, wallet)}>Verify Ticket</Button>
         </CardContent>
       </Card>
   );
