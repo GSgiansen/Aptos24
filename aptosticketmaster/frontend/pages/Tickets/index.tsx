d125131da071db502c52fea480d07631ad3335ba
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2024-07-17 00:43:32 +0800

    Add reload for tickets page

--- aptosticketmaster/frontend/pages/Tickets/index.tsx
+++ aptosticketmaster/frontend/pages/Tickets/index.tsx
@@ -1,17 +1,28 @@
+import { useEffect, useState } from "react";
 import { getEventsAndTickets, Ticket } from "@/hooks/getEventsAndTickets";
 import TicketCard from "@/pages/Tickets/TicketCard";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 export function Tickets() {
-  const wallet = useWallet();
-  const { tickets } = getEventsAndTickets(wallet);
+  const wallet = useWallet(); // Ensure this is directly within the functional component
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    async function fetchTickets() {
+      if (wallet) {
+        const data = await getEventsAndTickets(wallet);
+        setTickets(data.tickets);
+      }
+    }
+
+    fetchTickets();
+  }, [wallet]);
 
   return (
     <div>
-      {tickets.map((ticket) => {
-        return (
-          <TicketCard key={ticket.id} {...ticket} />
-        )})};
+      {tickets.map((ticket) => (
+        <TicketCard key={ticket.id} {...ticket} />
+      ))}
     </div>
   );
 }
